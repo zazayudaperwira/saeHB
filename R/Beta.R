f5b6a597e3da6d4702e4205d198d3e6e19e66120
zazaperwira <zazayudaperwira93@gmail.com>
2021-10-25 21:58:48 +0700

    first commit

---
+++ R/Beta.R
@@ -0,0 +1,224 @@
+#' @title Small Area Estimation using Hierarchical Bayesian under Beta Distribution
+#' @description This function is implemented to variable of interest \eqn{(y)} that assumed to be a Beta Distribution. The range of data must be \eqn{1<y<1}. The data proportion is supposed to be implemented with this function.
+#' @param formula Formula that describe the fitted model
+#' @param iter.update Number of updates with default \code{3}
+#' @param iter.mcmc Number of total iterations per chain with default \code{2000}
+#' @param thin Thinning rate, must be a positive integer with default \code{1}
+#' @param burn.in Number of iterations to discard at the begining with default \code{1000}
+#' @param data The data frame
+#'
+#' @return This function returns a list of the following objects:
+#'    \item{Est}{A vector with the values of Hierarchical Bayesian Estimators}
+#'    \item{sd}{A vector with the values of standar error of Hierarchical Bayesian Estimators}
+#'    \item{refVar}{Estimated random effect variances}
+#'    \item{coefficient}{A dataframe with the estimated model coefficient}
+#'    \item{plot}{Trace, Dencity, Autocorrelation Function Plot of MCMC samples}
+#' @export
+#'
+#' @examples
+Beta <- function(formula, iter.update=3, iter.mcmc=2000, thin = 1, burn.in =1000, data){
+
+  result <- list(Est = NA, sd = NA, refVar = NA, coefficient = NA,
+                 plot=NA)
+
+  formuladata <- model.frame(formula,data,na.action=NULL)
+  if (any(is.na(formuladata[,-1])))
+    stop("Auxiliary Variables contains NA values.")
+  #check apakah ada NA value atau 0, kalau ada ganti ke NA
+  formuladata[,1] <- ifelse(formuladata[,1]==0,NA,formuladata[,1])
+  formuladata[,1] <- ifelse(formuladata[,1]==1,0.999,formuladata[,1])
+
+
+  #Running untuk tersampel semua
+  if (!any(is.na(formuladata[,1])) && !any(formuladata[,1]==0)  ){ #Opening "for" Tersampel saja
+    formuladata <- as.matrix(na.omit(formuladata))
+    x <- model.matrix(formula,data = as.data.frame(formuladata))
+    x <- as.matrix(x)
+    n <- nrow(formuladata)
+    nvar <- ncol(formuladata)
+    mu.b =rep(0,nvar)
+    tau.b = rep(1,nvar)
+    tau.ub = tau.ua = phi.a = phi.b = a.var = 1
+
+    for(iter in 1:iter.update){
+
+      dat <- list("n"=n,"nvar"=nvar,"y"=formuladata[,1],"x"=as.matrix(formuladata[,2:nvar]),"mu.b"=mu.b,
+                  "tau.b"=tau.b,"tau.ua"=tau.ua, "tau.ub"=tau.ub, "phi.a"=phi.a, "phi.b"=phi.b) #Names List of number
+
+      inits <- list(phi=1, u=rep(0,n), b = mu.b, tau.u = 1)
+
+
+
+      cat("model{
+					for (i in 1:n) {
+							y[i] ~ dbeta(A[i],B[i])
+							A[i] <- mu[i] * phi
+              B[i] <- (1-mu[i]) * phi
+              logit(mu[i]) <- b[1] + sum(b[2:nvar]*x[i,]) + u[i]
+							u[i] ~ dnorm(0,tau.u)
+					}
+
+					for (k in 1:nvar){
+					    b[k] ~ dnorm(mu.b[k],tau.b[k])
+					}
+
+					phi ~ dgamma(phi.a,phi.b)
+					tau.u ~ dgamma(tau.ua,tau.ub)
+					a.var <- 1 / tau.u
+
+			}", file="saeHBbeta.txt")
+
+      jags.m <- jags.model(file = "saeHBbeta.txt", data=dat, inits=inits, n.chains=1, n.adapt=500)
+
+      params <- c("mu","a.var","b","phi","tau.u")
+      samps  <- coda.samples( jags.m, params, n.iter=iter.mcmc, thin=thin)
+      samps1 <- window(samps,start = burn.in+1, end =iter.mcmc )
+      result_samps=summary(samps1)
+      a.var=result_samps$statistics[1]
+      beta=result_samps$statistics[2:(nvar+1),1:2]
+      for (i in 1:nvar){
+        mu.b[i]  = beta[i,1]
+        tau.b[i] = 1/(beta[i,2]^2)
+      }
+
+      phi.a = result_samps$statistics[2+nvar+n,1]^2/result_samps$statistics[2+nvar+n,2]^2
+      phi.a = result_samps$statistics[2+nvar+n,1]/result_samps$statistics[2+nvar+n,2]^2
+      tau.ua = result_samps$statistics[3+nvar+n,1]^2/result_samps$statistics[3+nvar+n,2]^2
+      tau.ub = result_samps$statistics[3+nvar+n,1]/result_samps$statistics[3+nvar+n,2]^2
+
+
+    }#closing for Iteration update
+    result_samps=summary(samps1)
+    b.varnames <- list()
+    for (i in 1:(nvar)) {
+      idx.b.varnames <- as.character(i-1)
+      b.varnames[i] <-str_replace_all(paste("b[",idx.b.varnames,"]"),pattern=" ", repl="")
+    }
+
+    #Untuk Plot dan b.varnames
+    result_mcmc <- samps1[,c(2:(nvar+1))]
+    colnames(result_mcmc[[1]]) <- b.varnames
+
+    a.var=result_samps$statistics[1]
+
+    beta=result_samps$statistics[2:(nvar+1),1:2]
+    rownames(beta) <- b.varnames
+
+    mu=result_samps$statistics[(nvar+2):(1+nvar+n),1:2]
+
+    Estimation=data.frame(mu)
+    colnames(Estimation)=c("mean","sd")
+
+    Quantiles <- as.data.frame(result_samps$quantiles[1:(2+nvar+n),])
+    q_beta <- (Quantiles[2:(nvar+1),])
+    rownames(q_beta) <- b.varnames
+    beta <- cbind(beta,q_beta)
+
+  }
+
+  else {   # Area Non sample
+    formuladata <- as.data.frame(formuladata)
+    #x <- as.matrix(formuladata)
+    n <- nrow(formuladata)
+    nvar <- ncol(formuladata)
+    mu.b =rep(0,nvar)
+    tau.b = rep(1,nvar)
+    tau.ub = tau.ua = phi.a = phi.b = a.var = 1
+
+    formuladata$idx <- rep(1:n)
+    data_sampled <- na.omit(formuladata)
+    data_nonsampled <- formuladata[-data_sampled$idx,]
+
+    r=data_nonsampled$idx
+    n1=nrow(data_sampled)
+    n2=nrow(data_nonsampled)
+
+
+
+    for(iter in 1:iter.update){
+      dat <- list("n1" = n1,"n2"=n2,"nvar"=nvar,"y_sampled" = data_sampled[,1], "x_sampled"=data_sampled[,2:nvar],
+                  "x_nonsampled"=data_nonsampled[,2:nvar],"mu.b"=mu.b,
+                  "tau.b"=tau.b, "tau.ua"=tau.ua,"tau.ub"=tau.ub, "phi.a"=phi.a,"phi.b"=phi.b)
+
+      inits <- list(phi=1,u = rep(0,n1),v = rep(0,n2), b = mu.b, tau.u = 1)
+
+      cat("model {
+					for (i in 1:n1) {
+							y_sampled[i] ~ dbeta(A[i],B[i])
+							A[i] <- mu[i] * phi
+              B[i] <- (1-mu[i]) * phi
+              logit(mu[i]) <- b[1] +  sum(b[2:nvar]*x_sampled[i,]) + u[i]
+							u[i] ~ dnorm(0,tau.u)
+
+					}
+
+					for (j in 1:n2) {
+					v[j]~dnorm(0,tau.u)
+            logit(mu.nonsampled[j]) <- b[1] +sum(b[2:nvar]*x_nonsampled[j,])+v[j]
+          }
+
+					for (k in 1:nvar){
+					    b[k] ~ dnorm(mu.b[k],tau.b[k])
+					}
+
+					phi ~ dgamma(phi.a,phi.b)
+					tau.u ~ dgamma(tau.ua,tau.ub)
+					a.var <- 1 / tau.u
+
+			}", file="saeHBbeta.txt")
+
+      jags.m <- jags.model( file = "saeHBbeta.txt", data=dat, inits=inits, n.chains=1, n.adapt=500 )
+      params <- c("mu","mu.nonsampled","a.var","b", "phi", "tau.u")
+      samps <- coda.samples( jags.m, params, n.iter=iter.mcmc, thin=thin)
+      samps1 <- window(samps,start = burn.in+1, end =iter.mcmc )
+      result_samps=summary(samps1)
+      a.var=result_samps$statistics[1]
+      beta=result_samps$statistics[2:(nvar+1),1:2]
+      for (i in 1:nvar){
+        mu.b[i]  = beta[i,1]
+        tau.b[i] = 1/(beta[i,2]^2)
+      }
+      phi.a = result_samps$statistics[2+nvar+n,1]^2/result_samps$statistics[2+nvar+n,2]^2
+      phi.a = result_samps$statistics[2+nvar+n,1]/result_samps$statistics[2+nvar+n,2]^2
+      tau.ua = result_samps$statistics[3+nvar+n,1]^2/result_samps$statistics[3+nvar+n,2]^2
+      tau.ub = result_samps$statistics[3+nvar+n,1]/result_samps$statistics[3+nvar+n,2]^2
+
+    } #closinf for non sample iteration
+    result_samps=summary(samps1)
+    b.varnames <- list()
+    for (i in 1:(nvar)) {
+      idx.b.varnames <- as.character(i-1)
+      b.varnames[i] <-str_replace_all(paste("b[",idx.b.varnames,"]"),pattern=" ", repl="")
+    }
+
+    #Untuk Plot dan b.varnames
+    result_mcmc <- samps1[,c(2:(nvar+1))]
+    colnames(result_mcmc[[1]]) <- b.varnames
+
+    a.var=result_samps$statistics[1]
+
+    beta=result_samps$statistics[2:(nvar+1),1:2]
+    rownames(beta) <- b.varnames
+
+    mu=result_samps$statistics[(nvar+2):(1+nvar+n1),1:2]
+    mu.nonsampled =result_samps$statistics[(2+nvar+n1):(1+nvar+n),1:2]
+
+    Estimation=matrix(rep(0,n),n,2)
+    Estimation[r,]=mu.nonsampled
+    Estimation[-r,]=mu
+    Estimation = as.data.frame(Estimation)
+    colnames(Estimation)=c("mean","sd")
+
+    Quantiles <- as.data.frame(result_samps$quantiles[1:(2+nvar+n),])
+    q_beta <- (Quantiles[2:(nvar+1),])
+    rownames(q_beta) <- b.varnames
+    beta <- cbind(beta,q_beta)
+  } #closing else
+
+  result$Est = Estimation$mean
+  result$sd         = Estimation$sd
+  result$refVar      = a.var
+  result$coefficient = beta
+  result$plot       = list(graphics.off() ,par(mar=c(2,2,2,2)),autocorr.plot(result_mcmc,col="brown2",lwd=2),plot(result_mcmc,col="brown2",lwd=2))
+  return(result)
+}
---
+++ R/Normal.R
@@ -0,0 +1,231 @@
+#' @title Small Area Estimation using Hierarchical Bayesian under Normal Distribution
+#' @description This function is implemented to variable of interest \eqn{(y)} that assumed to be a Normal Distribution. The range of data is \eqn{(-\infty < y < \infty)}
+#' @param formula Formula that describe the fitted model
+#' @param vardir Sampling variances of direct estimations
+#' @param iter.update Number of updates with default \code{3}
+#' @param iter.mcmc Number of total iterations per chain with default \code{2000}
+#' @param thin Thinning rate, must be a positive integer with default \code{1}
+#' @param burn.in Number of iterations to discard at the begining with default \code{1000}
+#' @param data The data frame
+#'
+#' @return
+#' @export
+#'
+#' @examples
+Normal <- function(formula, vardir, iter.update=3, iter.mcmc=2000, thin = 1, burn.in =1000, data){
+
+
+  result <- list(Est = NA, sd = NA, refVar = NA, coefficient = NA,
+                 plot=NA)
+
+  formuladata <- model.frame(formula,data,na.action=NULL)
+  if (any(is.na(formuladata[,-1])))
+    stop("Auxiliary Variables contains NA values.")
+  auxVar <- formuladata[,-1]
+  nvar <- ncol(auxVar) + 1
+  formuladata <- data.frame(formuladata,vardir = data[,vardir])
+
+  for (i in 1:nrow(formuladata)) {
+    if (!is.na(formuladata[i,1])){
+      if (is.na(formuladata[i,(nvar+1)])){
+        stop(formula[2],"[",i,"] is not NA but vardir is NA")
+      }
+    }
+  }
+
+
+  if (!any(is.na(formuladata[,1])))  { #Opening "for" Tersampel saja
+
+
+    formuladata <- as.matrix(na.omit(formuladata))
+    x <- model.matrix(formula,data = as.data.frame(formuladata))
+    n <- nrow(formuladata)
+    for(i in 1:n){
+      if(formuladata[i,(nvar+1)]==0){
+        stop("Vardir for ", formula[2],"[",i,"] must not be 0"  )}
+    }
+    mu.b =rep(0,nvar)
+    tau.b = rep(1,nvar)
+    tau.ua = tau.ub = a.var = 1
+
+
+    for (i in 1:iter.update) {
+      dat <- list("n" = n,"nvar"=nvar,"y" = formuladata[,1], "x"=x[,-1],"mu.b"=mu.b,"tau.b"=tau.b,
+                  "tau.ua"=tau.ua,"tau.ub"=tau.ub, "vardir"=formuladata[,(nvar+1)])  # names list of numbers
+      inits <- list(u = rep(0,n), b = mu.b, tau.u = 1)
+      cat("model {
+					for (i in 1:n) {
+							y[i] ~ dnorm(mu[i],tau[i])
+							mu[i] <- b[1] + sum(b[2:nvar]*x[i,])   + u[i]
+
+							tau[i] <- 1/vardir[i]
+							u[i] ~ dnorm(0,tau.u)
+					}
+
+					for (k in 1:nvar){
+					    b[k] ~ dnorm(mu.b[k],tau.b[k])
+					}
+
+					tau.u ~ dgamma(tau.ua,tau.ub)
+					a.var <- 1 / tau.u
+
+			}", file="saeHBnormal.txt")
+
+      jags.m <- jags.model( file = "saeHBnormal.txt", data=dat, inits=inits, n.chains=1, n.adapt=500 )
+      params <- c("mu","a.var","b", "tau.u")
+      samps <- coda.samples( jags.m, params, n.iter=iter.mcmc, thin=thin)
+      samps1 <- window(samps, start=burn.in+1, end=iter.mcmc)
+
+      result_samps=summary(samps1)
+      a.var=result_samps$statistics[1]
+      beta=result_samps$statistics[2:(nvar+1),1:2]
+      for (i in 1:nvar){
+        mu.b[i]  = beta[i,1]
+        tau.b[i] = 1/(beta[i,2]^2)
+      }
+      tau.ua = result_samps$statistics[2+nvar+n,1]^2/result_samps$statistics[2+nvar+n,2]^2
+      tau.ub = result_samps$statistics[2+nvar+n,1]/result_samps$statistics[2+nvar+n,2]^2
+
+
+    } #closing of iteration update for Sampled Area
+
+    result_samps=summary(samps1)
+    b.varnames <- list()
+    for (i in 1:(nvar)) {
+      idx.b.varnames <- as.character(i-1)
+      b.varnames[i] <-str_replace_all(paste("b[",idx.b.varnames,"]"),pattern=" ", repl="")
+    }
+
+    #Untuk Plot dan b.varnames
+    result_mcmc <- samps1[,c(2:(nvar+1))]
+    colnames(result_mcmc[[1]]) <- b.varnames
+
+    a.var=result_samps$statistics[1]
+
+    beta=result_samps$statistics[2:(nvar+1),1:2]
+    rownames(beta) <- b.varnames
+
+    mu=result_samps$statistics[(nvar+2):(1+nvar+n),1:2]
+
+    Estimation=data.frame(mu)
+    colnames(Estimation)=c("mean","sd")
+
+    Quantiles <- as.data.frame(result_samps$quantiles[1:(2+nvar+n),])
+    q_beta <- (Quantiles[2:(nvar+1),])
+    rownames(q_beta) <- b.varnames
+    beta <- cbind(beta,q_beta)
+
+  }
+
+  else {   # Area Non sample
+
+    formuladata <- as.data.frame(formuladata)
+
+    x <- as.matrix(formuladata[,2:nvar])
+    n <- nrow(formuladata)
+
+    mu.b =rep(0,nvar)
+    tau.b = rep(1,nvar)
+    tau.ua = tau.ub = a.var = 1
+
+    formuladata$idx <- rep(1:n)
+    data_sampled <- na.omit(formuladata)
+    data_nonsampled <- formuladata[-data_sampled$idx,]
+
+    r=data_nonsampled$idx
+    n1=nrow(data_sampled)
+    n2=nrow(data_nonsampled)
+    for(i in 1:n1){
+      if(data_sampled[i,(nvar+1)]==0){
+        stop("Vardir for ",formula[2],"[",data_sampled$idx[i],"] must not be 0"  )}
+    }
+
+
+
+    for (i in 1:iter.update) {
+      dat <- list("n1" = n1,"n2"=n2,"nvar"=nvar,"y_sampled" = data_sampled[,1], "x_sampled"=data_sampled[,2:nvar],
+                  "x_nonsampled"=data_nonsampled[,2:nvar],"mu.b"=mu.b,
+                  "tau.b"=tau.b, "tau.ua"=tau.ua,"tau.ub"=tau.ub,"vardir"=data_sampled$vardir)
+
+      inits <- list(u = rep(0,n1), v = rep(0,n2), b = mu.b, tau.u = 1)
+
+      cat("model {
+					for (i in 1:n1) {
+							y_sampled[i] ~ dnorm(mu[i],tau[i])
+							mu[i] <- b[1] + sum(b[2:nvar]*x_sampled[i,])  +u[i]
+							tau[i] <- 1/vardir[i]
+							u[i] ~ dnorm(0,tau.u)
+					}
+
+					for (j in 1:n2) {
+					v[j]~dnorm(0,tau.u)
+            mu.nonsampled[j] <- b[1] + sum(b[2:nvar]*x_nonsampled[j,]) +v[j]
+          }
+
+					for (k in 1:nvar){
+					    b[k] ~ dnorm(mu.b[k],tau.b[k])
+					}
+
+					tau.u ~ dgamma(tau.ua,tau.ub)
+					a.var <- 1 / tau.u
+
+			}", file="saeHBnormal.txt")
+
+      jags.m <- jags.model( file = "saeHBnormal.txt", data=dat, inits=inits, n.chains=1, n.adapt=500 )
+      params <- c("mu","mu.nonsampled","a.var","b", "tau.u")
+      samps <- coda.samples( jags.m, params, n.iter=iter.mcmc, thin=thin)
+      samps1 <- window(samps, start=burn.in+1, end=iter.mcmc)
+
+      result_samps=summary(samps1)
+      a.var=result_samps$statistics[1]
+      beta=result_samps$statistics[2:(nvar+1),1:2]
+      for (i in 1:nvar){
+        mu.b[i]  = beta[i,1]
+        tau.b[i] = 1/(beta[i,2]^2)
+      }
+      tau.ua = result_samps$statistics[2+nvar+n,1]^2/result_samps$statistics[2+nvar+n,2]^2
+      tau.ub = result_samps$statistics[2+nvar+n,1]/result_samps$statistics[2+nvar+n,2]^2
+    }
+    result_samps=summary(samps1)
+    b.varnames <- list()
+    for (i in 1:(nvar)) {
+      idx.b.varnames <- as.character(i-1)
+      b.varnames[i] <-str_replace_all(paste("b[",idx.b.varnames,"]"),pattern=" ", repl="")
+    }
+
+    #Untuk Plot dan b.varnames
+    result_mcmc <- samps1[,c(2:(nvar+1))]
+    colnames(result_mcmc[[1]]) <- b.varnames
+
+    a.var=result_samps$statistics[1]
+
+    beta=result_samps$statistics[2:(nvar+1),1:2]
+    rownames(beta) <- b.varnames
+
+    mu=result_samps$statistics[(nvar+2):(1+nvar+n1),1:2]
+    mu.nonsampled =result_samps$statistics[(2+nvar+n1):(1+nvar+n),1:2]
+
+    Estimation=matrix(rep(0,n),n,2)
+    Estimation[r,]=mu.nonsampled
+    Estimation[-r,]=mu
+    Estimation = as.data.frame(Estimation)
+    colnames(Estimation)=c("mean","sd")
+
+    summary(Estimation$sd/Estimation$mean*100)
+
+    Quantiles <- as.data.frame(result_samps$quantiles[1:(2+nvar+n),])
+    q_beta <- (Quantiles[2:(nvar+1),])
+    rownames(q_beta) <- b.varnames
+    beta <- cbind(beta,q_beta)
+
+  } #closing else
+
+
+  result$Est = Estimation$mean
+  result$sd         = Estimation$sd
+  result$refVar      = a.var
+  result$coefficient = beta
+  result$plot       = list(graphics.off() ,par(mar=c(2,2,2,2)),autocorr.plot(result_mcmc,col="brown2",lwd=2),plot(result_mcmc,col="brown2",lwd=2))
+  return(result)
+
+}
